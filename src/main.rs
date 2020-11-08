f61ebc44e75e760eb99ad58a5315ccaf0971fe36
Toshiya Nakakura <nakakura@me.com>
2020-11-08 18:22:43 +0900

    add suffix "-release" to CMakeList

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,14 @@ fn main() {
     // Initialize node
     rosrust::init("talker");
 
-    // Create publisher
-    let chatter_pub = rosrust::publish("chatter", 100).unwrap();
-
     let mut count = 0;
-
     // Create object that maintains 10Hz between sleep requests
     let rate = rosrust::rate(10.0);
 
     // Breaks when a shutdown signal is sent
     while rosrust::is_ok() {
-        // Create string message
-        let mut msg = rosrust_msg::std_msgs::String::default();
-        msg.data = format!("hello world {}", count);
-
-        // Send string message to topic via publisher
-        chatter_pub.send(msg).unwrap();
-
-        // Sleep to maintain 10Hz rate
+        rosrust::ros_info!("hello world {}", count);
         rate.sleep();
-
         count += 1;
     }
 }
